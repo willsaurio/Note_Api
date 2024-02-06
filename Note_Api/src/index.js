e621890772343a32bee4319c7afb93fe26e1b17f
will <71905471+willsaurio@users.noreply.github.com>
2024-02-06 12:44:30 -0600

    Add files via upload

---
+++ Note_Api/src/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const userRouter = require("./routes/userRoute");
+const noteRouter = require("./routes/noteRoute");
+
+app.use(express.json());
+
+app.use("./user", userRouter);
+app.use("./note", noteRouter);
+
+app.get("/",(req, res) =>{
+    res.send("Bienvenido aplicacion de notas API");
+});
+
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:zpIVkAj69bBqbPGQ@cluster0.a5snalu.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() =>
+    app.listen(3000, () => console.log("Server started on post no: 3000"))
+  )
+  .catch((err) => console.log(err.message));
+ 
